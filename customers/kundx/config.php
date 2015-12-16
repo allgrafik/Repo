2897454e2854a31bf709ac4b51ef5dd93a2250c5
root <root@wnportal.brandfactory.local>
2015-12-16 16:38:52 +0100

    "initial commit for brandeasy customers configurations"

---
+++ customers/kundx/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * Customer configuration (config.php)
+ *
+ * - Use this file to override $config settings for this customer. You can
+ *   override ANY setting here so be specific and be careful!
+ *
+ * - The default lines below HIDE all webshops by default. Comment out or 
+ *   remove the corresponding webshop line for that webshop to be shown.
+ */
+
+// Hide Monitor webshop
+$config['vcmp']['pages']['home']['content']['webshop'] = null;
+
+// Hide NAV webshop
+// $config['vcmp']['pages']['home']['content']['nav'] = null;
+
+// Hide Visma webshop
+$config['vcmp']['pages']['home']['content']['visma'] = null;
+
+// Hide workflow
+$config['vcmp']['pages']['home']['content']['workflow'] = null;
